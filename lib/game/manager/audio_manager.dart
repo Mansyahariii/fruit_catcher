19247744f0c1fbac9125229c1356d1f28bbcd2c2
Mansyahariii <arifirmansyah350@gmail.com>
2026-02-13 15:59:53 +0700

    membuat void playSfxWithVolume

--- lib/game/manager/audio_manager.dart
+++ lib/game/manager/audio_manager.dart
@@ -81,4 +81,16 @@ class AudioManager {
       }
     }
   }
+
+  /// Play sound effect with custom volume
+  void playSfxWithVolume(String fileName, double volume) {
+    if (_isSfxEnabled) {
+      try {
+        final adjustedVolume = (volume * _sfxVolume).clamp(0.0, 1.0);
+        FlameAudio.play('sfx/$fileName', volume: adjustedVolume);
+      } catch (e) {
+        print('Error playing SFX with volume: $e');
+      }
+    }
+  }
 }
