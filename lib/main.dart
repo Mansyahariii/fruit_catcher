fd0b47f24acc260a0be8d229c5bc7e8092fb7d9e
Mansyahariii <arifirmansyah350@gmail.com>
2026-02-13 15:15:11 +0700

    wrap stack ke expanded

--- lib/main.dart
+++ lib/main.dart
@@ -31,51 +31,53 @@ class _GameScreenState extends State<GameScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            top: 50,
-            left: 20,
-            child: Container(
-              padding: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Colors.black54,
-                borderRadius: BorderRadius.circular(10),
-              ),
-
-              child: ValueListenableBuilder(
-                valueListenable: counter,
-                builder: (context, score, child) {
-                  return Text(
-                    "Score: $score",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  );
-                },
+      body: Expanded(
+        child: Stack(
+          children: [
+            Positioned(
+              top: 50,
+              left: 20,
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.black54,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+        
+                child: ValueListenableBuilder(
+                  valueListenable: counter,
+                  builder: (context, score, child) {
+                    return Text(
+                      "Score: $score",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
-          ),
-
-          Positioned(
-            top: 50,
-            right: 20,
-            child: Row(
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.music_note, color: Colors.black),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: const Icon(Icons.volume_up, color: Colors.black),
-                  onPressed: () {},
-                ),
-              ],
+        
+            Positioned(
+              top: 50,
+              right: 20,
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.music_note, color: Colors.black),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.volume_up, color: Colors.black),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
